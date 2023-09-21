0e52b11d9db0166010ca6cf9a3e756633864f568
brunocriacoes <br.rafael@outlook.com>
2023-09-21 14:57:14 -0300

    ajust trechos

--- trechos/add_menu_store.php
+++ trechos/add_menu_store.php
@@ -1,7 +1,8 @@
 <?php
 
-add_filter('wcfm_menus','ws_get_wcfm_menus_b', 30, 1);
-function ws_get_wcfm_menus_b($wcfm_menus) {
+add_filter('wcfm_menus', 'ws_get_wcfm_menus_b', 30, 1);
+function ws_get_wcfm_menus_b($wcfm_menus)
+{
 	$wcfm_menus['ws-contract-1'] = array(
 		'label' => 'carteira',
 		'url' => 'http://carteira.digitalcombo.com.br/public/',
--- trechos/create_user.php
+++ trechos/create_user.php
@@ -1,43 +1,30 @@
 <?php
 
-function enviar_dados_para_webhook($user_id) {
-	
-	
-    //     Verifica se o usuário foi criado com sucesso
-        if (!is_wp_error($user_id)) {
-            $user = get_user_by('ID', $user_id);
-            
-            // Verifique se o usuário existe
-            if ($user) {
-                 $webhook_url = 'https://n8n.digitalcombo.com.br/webhook/abd89804-cb9c-450d-bb43-4bbd34a742f4';
-    
-                // Dados a serem enviados para o webhook
-                $data = array(
-                    'user_id' => $user_id,
-                    'user_email' => $user->user_email,
-                    'user_name' => $user->user_login,
-                    'user_password' => $_POST['pass1'],
-                    'timestamp' => current_time('mysql'),
-                );
-    
-                // Configuração da requisição POST
-                $args = array(
-                    'body' => json_encode($data),
-                    'headers' => array(
-                        'Content-Type' => 'application/json',
-                    ),
-                );
-    
-                // Envia a requisição POST para o webhook
-                $response = wp_safe_remote_post($webhook_url, $args);
-    
-                // Verifica se a requisição foi bem-sucedida (você pode personalizar essa verificação)
-                if (is_wp_error($response)) {
-                    error_log('Erro ao enviar dados para o webhook: ' . $response->get_error_message());
-                }
+function enviar_dados_para_webhook($user_id)
+{
+    if (!is_wp_error($user_id)) {
+        $user = get_user_by('ID', $user_id);
+        if ($user) {
+            $webhook_url = 'https://n8n.digitalcombo.com.br/webhook/abd89804-cb9c-450d-bb43-4bbd34a742f4';
+            $data = array(
+                'user_id' => $user_id,
+                'user_email' => $user->user_email,
+                'user_name' => $user->user_login,
+                'user_password' => $_POST['pass1'],
+                'timestamp' => current_time('mysql'),
+            );
+            $args = array(
+                'body' => json_encode($data),
+                'headers' => array(
+                    'Content-Type' => 'application/json',
+                ),
+            );
+            $response = wp_safe_remote_post($webhook_url, $args);
+            if (is_wp_error($response)) {
+                error_log('Erro ao enviar dados para o webhook: ' . $response->get_error_message());
             }
         }
     }
-    
-    // Adicione um gancho para executar a função após o cadastro do usuário
-    add_action('user_register', 'enviar_dados_para_webhook');
+}
+
+add_action('user_register', 'enviar_dados_para_webhook');
